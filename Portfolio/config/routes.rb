c19c61957661d5a80c95a97ac1a7901ff6c5d86c
nyanzebra <robbaldwin95@gmail.com>
2016-02-11 20:50:32 -0600

    adds api configurations

--- Portfolio/config/routes.rb
+++ Portfolio/config/routes.rb
@@ -1,4 +1,10 @@
+require "api_constraints"
 Portfolio::Application.routes.draw do
+namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  end
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
