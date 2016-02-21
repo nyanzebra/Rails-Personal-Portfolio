ad87bd894a506445ec89c1e8530da1b1d926aa44
nyanzebra <robbaldwin95@gmail.com>
2016-02-21 14:20:44 -0600

    Removes old user scaffold

--- Portfolio/app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :firstname, :lastname, :role, :email, :created_at, :updated_at
--- Portfolio/config/routes.rb
+++ Portfolio/config/routes.rb
@@ -1,7 +1,5 @@
 require "api_constraints"
 Portfolio::Application.routes.draw do
-  resources :users
-
 namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
   end
